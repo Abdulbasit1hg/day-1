0581c7eece08adcbc684204a966a3211dcfcf0e2
Abdulbasit1hg <99991490+Abdulbasit1hg@users.noreply.github.com>
2022-04-01 15:20:26 +0500

    js commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+let x = "object"
+console.log(x);
+
+x = "family";
+console.log(x);
+
+let y = "school"
+console.log(y);
+
+y = "student";
+console.log(y);
+
+let hight = "lite";
+console.log(hight);
+
+hight = "meters";
+console.log(hight);
+
+// number data type
+
+let close = 123;
+console.log(close);
+
+let school = 43;
+console.log(school);
+
+let city = 12;
+console.log(city);  
+
+let ich = 18;
+console.log(ich);
+
+let get = 14;
+console.log(get);
+
+// bigint data type
+
+let biginT = 5363664674;
+console.log(biginT);
+
+biginT = 098172675;
+console.log(biginT)
+
+let more = 756489929;
+console.log(more);
+
+more = 847562534;
+console.log(more);
+
+// boolin data type
+
+let age = true;
+console.log(age);
+
+age = false;
+console.log(age)
+
+let fire = true;
+console.log(fire);
+
+fire = false;
+console.log(fire);
+
+// undefined data tiyp
+
+let hover;
+console.log(hover);
+
+let tover;
+console.log(tover)
+
+let fixed;
+console.log(fixed);
+
+let check;
+console.log(check);
+
+
+
+
+
